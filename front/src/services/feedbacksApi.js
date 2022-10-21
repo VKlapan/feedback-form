2acb419105ce8649530d2c7642c601d20ef74e66
VKlapan <vladimir.klapan@gmail.com>
2022-10-21 13:37:02 +0300

    refactoring docker-compose and dev stage configs

--- front/src/services/feedbacksApi.js
+++ front/src/services/feedbacksApi.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "/api",
+  //baseURL: "/api",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export const getFeedbacks = async () => {
